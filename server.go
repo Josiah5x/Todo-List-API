9d6308b4b4f27d1df2b0dc24e69567fa81ba473b
Josiah <josiahlivinuschuntan@gmail.com>
2023-04-12 11:56:53 +0100

    update route method

--- server.go
+++ server.go
@@ -3,11 +3,9 @@ package main
 import (
 	"github.com/Josiah5x/Todo-List-API/config"
 	"github.com/Josiah5x/Todo-List-API/controller"
+	"github.com/Josiah5x/Todo-List-API/pkg/mongodb"
 	mongodbd "github.com/Josiah5x/Todo-List-API/platform/mongodb_platform"
 	"github.com/Josiah5x/Todo-List-API/services"
-
-	"github.com/Josiah5x/Todo-List-API/pkg/mongodb"
-
 	echo "github.com/labstack/echo"
 )
 
@@ -22,17 +20,14 @@ func main() {
 	svc := services.New(mgos)
 	// Controller
 	control := controller.New(svc)
-
 	e := echo.New()
-
 	v1 := e.Group("/v1")
-
 	todo := v1.Group("/todo")
 
-	todo.POST("/todo", control.AddTodo)
-	todo.PUT("/todo/:id", control.UpdateTodo)
-	todo.GET("/todo", control.GetAllTodo)
-	todo.GET("/todo/:id", control.GetTodo)
+	todo.POST("", control.AddTodo)
+	todo.PUT(":id", control.UpdateTodo)
+	todo.GET("", control.GetAllTodo)
+	todo.GET(":id", control.GetTodo)
 	todo.PATCH("/mark/:id", control.MarkTodoUpdateDone)
 	todo.PATCH("/deadline/:id", control.ChangeTodoDeadline)
 	todo.DELETE("/delete/:id", control.DeleteTodo)
